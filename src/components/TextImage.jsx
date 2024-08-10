0e0fda0550c4a27fa65c332a63d62e96698155ce
Ayush <asharma7588@gmail.com>
2024-08-10 16:37:51 +0530

    added props in images

--- src/components/TextImage.jsx
+++ src/components/TextImage.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "../TextImage.css";
 
-const TextImage = ({ imageUrl, className, children }) => {
+const TextImage = ({
+  imageUrl,
+  className = "",
+  cover = false,
+  pos = "0%",
+  children,
+}) => {
   const style = {
     backgroundImage: `url(${imageUrl})`,
+    backgroundPosition: pos,
+    backgroundSize: cover ? "cover" : "auto",
   };
 
   return (
-    <span style={style} className={`text-image ${className || ""}`}>
+    <span style={style} className={`text-image ${className}`}>
       {children}
     </span>
   );
