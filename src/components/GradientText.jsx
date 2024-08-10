0b378f09a197356799486b091046c43ade5a0ec5
Ayush <asharma7588@gmail.com>
2024-08-10 19:43:32 +0530

    gradient update

--- src/components/GradientText.jsx
+++ src/components/GradientText.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "../styles.css";
 
-const GradientText = ({ color, className, children }) => {
+const GradientText = ({ color, className, children, ...props }) => {
   const ClassName = `${color ? color.toLowerCase() : ""} ${className || ""}`;
 
-  return <span className={ClassName}>{children}</span>;
+  return (
+    <span className={ClassName} {...props}>
+      {children}
+    </span>
+  );
 };
 
 export default GradientText;
